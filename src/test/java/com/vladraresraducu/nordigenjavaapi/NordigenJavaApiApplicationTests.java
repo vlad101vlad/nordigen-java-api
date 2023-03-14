c0e3ead7c61a8c40fe5dc63b67f0360555fc5007
Raducu Vlad-Rares <vlad101vlad@gmail.com>
2023-03-14 21:21:09 +0200

    Spring project initial commit

---
+++ src/test/java/com/vladraresraducu/nordigenjavaapi/NordigenJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladraresraducu.nordigenjavaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NordigenJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
