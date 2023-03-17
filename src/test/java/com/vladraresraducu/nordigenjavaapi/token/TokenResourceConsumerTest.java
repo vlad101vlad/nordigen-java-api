1512864bd18026f0dd809a5206b8b0900518c21e
Raducu Vlad-Rares <vlad101vlad@gmail.com>
2023-03-17 21:07:02 +0200

    Separated testes for token endpoints

--- src/test/java/com/vladraresraducu/nordigenjavaapi/token/TokenResourceConsumerTest.java
+++ src/test/java/com/vladraresraducu/nordigenjavaapi/token/TokenResourceConsumerTest.java
@@ -2,9 +2,11 @@ package com.vladraresraducu.nordigenjavaapi.token;
 
 import com.vladraresraducu.nordigenjavaapi.token.model.JWTObtainPairRequest;
 import com.vladraresraducu.nordigenjavaapi.token.model.JWTRefreshRequest;
-import com.vladraresraducu.nordigenjavaapi.token.model.SpectacularJWTObtain;
 import com.vladraresraducu.nordigenjavaapi.token.model.SpectacularJWTRefresh;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,18 +23,21 @@ public class TokenResourceConsumerTest {
     @Autowired
     private TokenResourceConsumer tokenResourceConsumer;
 
-
+    @Order(1)
     @Test
     public void testV2TokenNew_shouldReturnJWT() {
-        SpectacularJWTObtain spectacularJWTObtain = tokenResourceConsumer.getNewToken(getJwtObtainPairRequest());
-        SpectacularJWTRefresh spectacularJWTRefresh = tokenResourceConsumer.refreshToken(new JWTRefreshRequest(spectacularJWTObtain.getRefresh()));
-
+        var spectacularJWTObtain = tokenResourceConsumer.getNewToken(getJwtObtainPairRequest());
         assertNotNull(spectacularJWTObtain);
+    }
+    @Order(2)
+    @Test
+    public void testV2RefreshToken_shouldReturnJWTRefresh() {
+        var spectacularJWTObtain = tokenResourceConsumer.getNewToken(getJwtObtainPairRequest());
+        SpectacularJWTRefresh spectacularJWTRefresh = tokenResourceConsumer.refreshToken(new JWTRefreshRequest(spectacularJWTObtain.getRefresh()));
         assertNotNull(spectacularJWTRefresh);
     }
 
     private JWTObtainPairRequest getJwtObtainPairRequest() {
-        System.out.println(this.secretId);
         return new JWTObtainPairRequest(this.secretId, this.secretKey);
     }
 }
